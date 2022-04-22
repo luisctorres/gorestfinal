b0caace015e3c7e156b56f202b3a6644b921726d
luisctorres <Scratch2c@gmail.com>
2022-04-21 20:57:26 -0400

    Added Utilities and Post ValidationError Class

---
+++ src/main/java/com/careerdevs/gorestfinal/models/Post.java
@@ -0,0 +1,38 @@
+package com.careerdevs.gorestfinal.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private String body;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/careerdevs/gorestfinal/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.careerdevs.gorestfinal.repositories;
+
+import com.careerdevs.gorestfinal.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long > {
+
+
+}
